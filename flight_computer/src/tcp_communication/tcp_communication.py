84210bf53df8a7d34090a45761965e00b7072edf
GoncaloR00 <gribeiro@ua.pt>
2022-06-25 04:37:23 +0100

    Mais uns ajustes

--- flight_computer/src/tcp_communication/tcp_communication.py
+++ flight_computer/src/tcp_communication/tcp_communication.py
@@ -4,22 +4,35 @@ import socket
 import threading
 import pickle
 import time
-
 import rospy
 from flight_computer.msg import TYPR
 
-topic_MessageToSend = '/serial/toSend'
-pub = rospy.Publisher(topic_MessageToSend, TYPR, queue_size=1)
+# -----------------------------------------
+# Parameters
+# -----------------------------------------
 
-# Connection Data
+# Connection
 host = socket.gethostname()
 port = 2022
 
-# Starting Server
+
+# -----------------------------------------
+# Initialization
+# -----------------------------------------
+
+# Topics
+topic_MessageToSend = '/serial/toSend'
+pub = rospy.Publisher(topic_MessageToSend, TYPR, queue_size=1)
+
+# Server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen(1)
 
+
+# -----------------------------------------
+# Functions
+# -----------------------------------------
 
 # Handling Messages From Clients and sending to the respective topics
 def handle(client):
@@ -31,16 +44,17 @@ def handle(client):
                 print('HELP')
                 # TODO meter led de alerta
                 break
-            aux1 = pickle.loads(message)
-            # print(type(aux1))
-            # throttle = aux1[0]
-            # print(throttle)
-            (throttle, yaw, pitch, roll, start, stop, sos, mode) = aux1
+            decoded = pickle.loads(message)
+            (throttle, yaw, pitch, roll, start, stop, sos, mode) = decoded
             msg = TYPR()
             msg.throttle = throttle
             msg.yaw = yaw
             msg.pitch = pitch
             msg.roll = roll
+            msg.start = bool(start)
+            msg.stop = bool(stop)
+            msg.sos = bool(sos)
+            msg.mode = bool(mode)
             pub.publish(msg)
             time.sleep(0.01)
 
